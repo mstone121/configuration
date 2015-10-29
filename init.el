c14a13ebb2caa8e5328b1e58be0e725202c06b8d
Matt Stone <mstone@mrn.org>
2015-10-29 21:38:14 +0000

    Spaces instead of tabs

--- init.el
+++ init.el
@@ -60,6 +60,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(indent-tabs-mode nil)
  '(tab-width 4)
  '(truncate-lines t))
 (custom-set-faces
