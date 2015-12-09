9e63584b346eb1f9f6f820e10647cbf9e69696c1
Matt Stone <mstone@mrn.org>
2015-12-09 21:22:15 +0000

    Added line for js mode

--- init.el
+++ init.el
@@ -26,6 +26,7 @@
 (add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
 (setq web-mode-code-indent-offset   4)
 (setq web-mode-css-indent-offset    4)
