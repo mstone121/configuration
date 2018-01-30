90644f11da612028fa08c2ec23ca9ed798ba7c2c
Matt Stone <mstone121@gmail.com>
2018-01-30 15:53:02 -0600

    Some emacs configuration

--- init.el
+++ init.el
@@ -1,2 +1,9 @@
+;; Package stuff
+(package-initialize)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/"))
+
 ;; Save backups elsewhere
 (setq backup-directory-alist '(("." . "~/emacs-saves")))
+
+;; whitespace bad
+(add-hook 'before-save-hook 'whitespace-cleanup)
